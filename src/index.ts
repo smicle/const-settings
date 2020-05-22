983f06b19e40cb5b80358c0958c0f5f32594ce45
smicle <bhhs1270026@gn.iwasaki.ac.jp>
2020-05-22 10:18:26 +0900

    Add コメントを追加

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import * as yaml from 'js-yaml'
 
 const yml = fs.readFileSync(`${process.cwd()}/config/settings.yaml`)
 const option = 'utf-8' as yaml.LoadOptions
+/**
+ * Structure of the contents of config/settings.yaml
+ */
 const Settings = yaml.safeLoad(String(yml), option)
 
 export default Settings
